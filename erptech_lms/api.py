8cf67c1a8234eaca685f1c303561976e4ed32211
Biswajit Sahoo <biswa4u85@gmail.com>
2023-12-20 03:25:47 +0530

    add files

--- erptech_lms/api.py
+++ erptech_lms/api.py
@@ -4,24 +4,35 @@ from frappe.utils import nowdate
  
  
 @frappe.whitelist(allow_guest=True)
-def getSalesInvoice(**kwargs):
+def create_sales_order(**kwargs):
 	data = list(frappe.form_dict.values())
+	print('data ', data)
  
 	# Create Customer
 	customer = None
-	exit_customer = frappe.get_value("Customer", filters={"customer_name": data[2]}, fieldname='name')
+	exit_customer = frappe.get_value("Customer", filters={"name": data[4]}, fieldname='name')
 	if exit_customer is None:
 		customer = frappe.new_doc('Customer')
 		customer.customer_group = "Individual"
 		customer.territory = "India"
-		customer.customer_name = data[2]
+		customer.customer_name = data[4]
+		customer.custom_customer_phone = data[5]
+		customer.custom_customer_email = data[4]
 		customer.insert(ignore_permissions=True)
 	
 	print("Customer Update")
-	# Create Sales Invoice
-	new_sales_invoice = frappe.new_doc("Sales Order")
-	new_sales_invoice.customer = exit_customer if customer is None else customer.name
-	new_sales_invoice.transaction_date = nowdate()
-	new_sales_invoice.set("items", [{"item_code": "Courses", "delivery_date": nowdate(), "qty": 1, "rate": data[3]}])
-	new_sales_invoice.insert(ignore_permissions=True)
+	# Create Sales Order
+	new_sales_order = frappe.new_doc("Sales Order")
+	new_sales_order.customer = exit_customer if customer is None else customer.name
+	new_sales_order.transaction_date = nowdate()
+	new_sales_order.set("items", [{"item_code": "Courses", "delivery_date": nowdate(), "qty": 1, "rate": data[2]}])
+	new_sales_order.insert(ignore_permissions=True)
 	print("invoice Created")
+ 
+	# Create Sales Order
+	# new_sales_order = frappe.new_doc("Sales Order")
+	# new_sales_order.customer = exit_customer if customer is None else customer.name
+	# new_sales_order.transaction_date = nowdate()
+	# new_sales_order.set("items", [{"item_code": "Courses", "delivery_date": nowdate(), "qty": 1, "rate": data[2]}])
+	# new_sales_order.insert(ignore_permissions=True)
+	# print("invoice Created")
